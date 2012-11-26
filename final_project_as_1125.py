45a4da66a9c0ddc5e1c32fa75dac57cac7afb5ce
Shaohan Chen <shaohan@ischool.berkeley.edu>
2012-11-26 00:24:03 -0800

    [Fixed] Remove unnecessary spaces

--- final_project_as_1125.py
+++ final_project_as_1125.py
@@ -13,18 +13,16 @@ class Graph(object):
             self.user_list.append(new_user)
 #output: user_list = [User0, User1, User2, ..., User(population - 1)]
 
-
     def circle_connect(self):
         """connect each user i to user i+1, and connect the last user to user 0"""
         for i in range(0, self.population - 1):
             self.user_list[i].add_friend(self.user_list[i + 1])
         self.user_list[self.population - 1].add_friend(self.user_list[0])
- 
+
 #output: user0.friends = [user(population-1), user1],
 #        user1.friends = [user0, user2],
 #        user2.friends = [user1, user3],
 #        ...user(population-1).friends = [user(population-2), user0]
-
 
     def random_connections(self, num_connections):
         """add num_connections(an assigned integer) new connections randomly to the graph"""
@@ -36,26 +34,21 @@ class Graph(object):
                 user2.add_friend(user1)
                 add_connection += 1
 
-    
     def time_step(self, prob):
         """move the simulation forward by one time period. For every user that has the new technology, the probability of passing it on to each friend is prob"""
         pass
 
-    
     def get_users(self):
         """returns a list containing the users in the graph, in order of their IDs"""
         return self.user_list
 
-    
-
-
 
 class User(object):
- 
+
     def __init__(self):
         if not hasattr(self, 'next_id'):
-            User.next_id = 0 
-        self.id = User.next_id 
+            User.next_id = 0
+        self.id = User.next_id
         User.next_id += 1
         self.is_adopted = False
         self.friends = []
@@ -63,12 +56,10 @@ class User(object):
     def __lt__(self, other):
         """this is a built in comparison function, so that User1 < User2 always return False. This is handy for sorting lists that contain users without throwing errors"""
         return self.id < other.id
-    
 
     def get_friends(self):
         """return a list of the user's friends, which are also users"""
         return self.friends
-
 
     def is_friend(self, other):
         """return True if this User and other are friends. return False otherwise"""
@@ -77,7 +68,6 @@ class User(object):
         else:
             return False
 
-
     def add_friend(self, other):
         if self == other:
             return
@@ -85,17 +75,14 @@ class User(object):
             self.friends.append(other)
             other.friends.append(self)
 
-
     def get_id(self):
         """return this user's id"""
         return self.id
-
 
     def has_adopted(self):
         """Returns True if the user has adopted the new technology. Returns False otherwise"""
         return self.is_adopted
 
-    
     def set_adopted(self, new_status):
         """If new_status is True, this user adopts the new technology. If new_status is False, this user becomes a non-adopter."""
         self.is_adopted = new_status
