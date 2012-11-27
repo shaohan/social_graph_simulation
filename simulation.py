fe60b20f1de5c87ff43875272d9a3473a09504de
Shaohan Chen <shaohan@ischool.berkeley.edu>
2012-11-26 22:56:51 -0800

    [Added] simulation.py: added 3 constants for simulation settings

--- simulation.py
+++ simulation.py
@@ -1,5 +1,9 @@
 import final_project_as_1125
 
+POPULATION = 4
+NUM_RANDOM_CONNECTIONS = 1
+NUM_FIRST_ADOPTERS = 2
+
 def print_users(graph):
     for user in graph.get_users():
         print('user ID:', user.id)
@@ -7,14 +11,18 @@ def print_users(graph):
             print('-- ', friend.id)
 
 print("Start testing")
-my_graph = final_project_as_1125.Graph(5)
+my_graph = final_project_as_1125.Graph(POPULATION)
 my_graph.circle_connect()
-my_graph.random_connections(1)
+my_graph.random_connections(NUM_RANDOM_CONNECTIONS)
 my_analyzer = final_project_as_1125.GraphAnalyzer(my_graph)
 
 print("=== get_users() test ===")
 print_users(my_graph)
 print_users(my_graph)
 
-my_analyzer.choose_users(20)
+print("Population:               ", POPULATION)
+print("Num_Random_Connections:   ", NUM_RANDOM_CONNECTIONS)
+print("Number of first adopters: ", NUM_FIRST_ADOPTERS)
+
+my_analyzer.choose_users(NUM_FIRST_ADOPTERS)
 #my_graph.print_users()
